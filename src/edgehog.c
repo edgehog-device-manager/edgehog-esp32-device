78de10ca8fe642b8c2de88cbc925d709f223311d
Francesco Vaiani <francesco.vaiani@secomind.com>
2021-11-05 21:42:57 +0100

    Publish Basic CPU Info
    Publish CPU model info to remote Astarte instance using io.edgehog.devicemanager.HardwareInfo interface.
    CPU info is retrieved using esp_chip_info function and published to /cpu/* endpoints.
    
    Signed-off-by: Francesco Vaiani <francesco.vaiani@secomind.com>

--- src/edgehog.c
+++ src/edgehog.c
@@ -17,5 +17,90 @@
  */
 
 #include "edgehog.h"
+#include "esp_system.h"
+#include <esp_err.h>
+#include <esp_heap_caps.h>
+#include <esp_log.h>
 
-void edgehog_init(astarte_device_handle_t astarte_device) {}
+static const char *TAG = "EDGEHOG";
+
+const static astarte_interface_t hardware_info_interface
+    = { .name = "io.edgehog.devicemanager.HardwareInfo",
+          .major_version = 0,
+          .minor_version = 1,
+          .ownership = OWNERSHIP_DEVICE,
+          .type = TYPE_PROPERTIES };
+
+esp_err_t add_interfaces(astarte_device_handle_t device)
+{
+    astarte_err_t ret;
+    ret = astarte_device_add_interface(device, &hardware_info_interface);
+
+    if (ret != ASTARTE_OK) {
+        ESP_LOGE(TAG, "Unable to add Astarte Interface ( %s ) error code: %d",
+            hardware_info_interface.name, ret);
+        return ESP_FAIL;
+    }
+    return ESP_OK;
+}
+
+void publish_device_hardware_info(astarte_device_handle_t device)
+{
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+
+    char *cpu_architecture = "Xtensa";
+    char *cpu_vendor = "Espressif Systems";
+    char *cpu_model;
+    char *cpu_model_name;
+    long mem_total_bytes;
+
+    switch (chip_info.model) {
+        case CHIP_ESP32:
+            cpu_model = "ESP32";
+            if (chip_info.cores == 1) {
+                cpu_model_name = "Single-core Xtensa LX6";
+            } else {
+                cpu_model_name = "Dual-core Xtensa LX6";
+            }
+            break;
+        case CHIP_ESP32S2:
+            cpu_model = "ESP32-S2";
+            cpu_model_name = "Single-core Xtensa LX7";
+            break;
+        case CHIP_ESP32S3:
+            cpu_model = "ESP32-S3";
+            cpu_model_name = "Dual-core Xtensa LX7";
+            break;
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 3, 1)
+        case CHIP_ESP32C3:
+            cpu_model = "ESP32-C3";
+            cpu_model_name = "Single-core 32-bit RISC-V";
+            break;
+#endif
+        default:
+            cpu_model = "GENERIC";
+            cpu_model_name = "Generic";
+    }
+
+    mem_total_bytes = (long) heap_caps_get_total_size(MALLOC_CAP_INTERNAL);
+#ifdef CONFIG_SPIRAM_USE
+    mem_total_bytes += (long) heap_caps_get_total_size(MALLOC_CAP_SPIRAM);
+#endif
+    astarte_device_set_string_property(
+        device, hardware_info_interface.name, "/cpu/architecture", cpu_architecture);
+    astarte_device_set_string_property(
+        device, hardware_info_interface.name, "/cpu/model", cpu_model);
+    astarte_device_set_string_property(
+        device, hardware_info_interface.name, "/cpu/modelName", cpu_model_name);
+    astarte_device_set_string_property(
+        device, hardware_info_interface.name, "/cpu/vendor", cpu_vendor);
+    astarte_device_set_longinteger_property(
+        device, hardware_info_interface.name, "/mem/totalBytes", mem_total_bytes);
+}
+
+void edgehog_init(astarte_device_handle_t astarte_device)
+{
+    ESP_ERROR_CHECK(add_interfaces(astarte_device));
+    publish_device_hardware_info(astarte_device);
+}
